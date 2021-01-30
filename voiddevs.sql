de4f8c67152b266158e3789294eefee2b55b28a1
iClaudette <76551996+iClaudette@users.noreply.github.com>
2021-01-30 20:17:35 +0300

    Add files via upload

---
+++ voiddevs.sql
@@ -0,0 +1,167 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 26 Ara 2020, 15:42:53
+-- Sunucu sürümü: 10.4.16-MariaDB
+-- PHP Sürümü: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `membership`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `admin`
+--
+CREATE DATABASE membership;
+USE membership;
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `adminname` varchar(256) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Tablo döküm verisi `admin`
+--
+
+INSERT INTO `admin` (`id`, `adminname`, `email`, `password`) VALUES
+(1, 'VoidDevs', 'admin@voiddevs.org', 'ee951c16aedfeb93e89810f4fa0eae88');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `tickets`
+--
+USE membership;
+CREATE TABLE `tickets` (
+  `id` int(12) NOT NULL,
+  `username` varchar(256) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `msg` text NOT NULL,
+  `value` varchar(15) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` enum('open','closed','resolved') NOT NULL DEFAULT 'open'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `tickets`
+--
+
+INSERT INTO `tickets` (`id`, `username`, `title`, `msg`, `value`, `email`, `created`, `status`) VALUES
+(1, 'Claudétte', 'VoidDevelopment', 'Welcome to Void Development!', 'Yüksek', 'support@voiddevs.org', '2020-12-26 17:06:51', 'closed');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `tickets_comments`
+--
+USE membership;
+CREATE TABLE `tickets_comments` (
+  `id` int(11) NOT NULL,
+  `ticket_id` int(11) NOT NULL,
+  `msg` text NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `tickets_comments`
+--
+
+INSERT INTO `tickets_comments` (`id`, `ticket_id`, `msg`, `created`) VALUES
+(1, 1, 'VoidDevs.org', '2020-11-08 05:40:09'),
+(31, 1, 'e', '2020-12-26 15:23:38'),
+(32, 1, 'y', '2020-12-26 15:31:57');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `users`
+--
+USE membership;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `is_activated` tinyint(1) NOT NULL,
+  `activation_code` varchar(16) DEFAULT NULL,
+  `fpassword_key` varchar(128) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `is_activated`, `activation_code`, `fpassword_key`) VALUES
+(0, 'Claudétte', 'curosbow@gmail.com', '$2y$10$fNixzyQ9xBhXcUeq.bCaZew4gqSw6a.1wkef99VkU1nk98yQz/0j2', 0, 'eee2bdcc24d3b28', NULL);
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `tickets`
+--
+ALTER TABLE `tickets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `tickets_comments`
+--
+ALTER TABLE `tickets_comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `tickets`
+--
+ALTER TABLE `tickets`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `tickets_comments`
+--
+ALTER TABLE `tickets_comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
